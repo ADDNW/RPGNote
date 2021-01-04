6b773bf70326d490cc1546679465366e12d6f173
DDNW <a11@dreger.poznan.pl>
2021-01-04 02:08:07 +0100

    added menu to app

--- note_window.py
+++ note_window.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 
 class Note_window(tk.Tk):
   def __init__(self):
@@ -16,31 +17,56 @@ class Note_window(tk.Tk):
     self.markers = {}
     self.NPCs = {}
 
-    #GUI objects
-    (
-      self.note_area,
-      self.marked_files_area,
-      self.marked_files_buttons,
-      self.top_panel_area,
-      self.write_top_panel,
-      self.read_top_panel
-    ) = self.createGUI()
+    #GUI objects - created in createGUI method
+    self.note_area = None
+    self.marked_files_area = None
+    self.top_panel_area = None
+
+    self.__create_GUI()
   
   #init
-  def createGUI(self):
-    return (None, None, None, None, None, None)
+  def __create_GUI(self):
+    self.__create_menu()
+    self.__create_top_panels()
+    self.__create_marked_area()
+    self.__create_content()
+
+  def __create_menu(self):
+    menubar = tk.Menu(self)
+    filemenu = tk.Menu(menubar, tearoff = 0)
+    filemenu.add_command(label = "New working directory", command = self.__read_working_directory)
+    self.bind("<Control-o>", self.__read_working_directory)
+    filemenu.add_command(label = "Save", command = self.__save_file)
+    self.bind("<Control-s>", self.__save_file)
+
+    menubar.add_cascade(label = "File", menu = filemenu)
+    self.config(menu = menubar)
+
+  def __create_top_panels(self):
+    pass
+
+  def __create_marked_area(self):
+    pass
+
+  def __create_content(self):
+    pass
 
   #file working
-  def read_working_directory(self):
+  def __read_working_directory(self):
     return ''
 
   def mark_files(self):
     return []
 
-  def open_file(self, name):
-    pass
-  
-  def save_file(self, name):
+  def open_file(self):
+    path = filedialog.askopenfilename()
+    if not path.startswith(self.working_directory):
+      #TODO warning about file is not part of this workspace
+      return None
+    with open(path, "r") as new_file:
+      self.note_area #TODO add line by line text to note_area
+
+  def __save_file(self):
     pass
 
   #markers insert
